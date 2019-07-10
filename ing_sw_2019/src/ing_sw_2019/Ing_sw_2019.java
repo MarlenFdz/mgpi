3588d9cd6c0b2600402d8690e347b904294995a3
Usuario <Usuario@A06>
2019-07-10 17:47:53 -0500

    Tabla del 12

---
+++ ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ing_sw_2019;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ing_sw_2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Tabla de multiplicar del 12:");
+
+        for (int i = 0; i <= 12; i++) {
+            System.out.print("12 * " + i + " = " + 12 * i + "\n");
+        }
+
+    }
+
+}
